cbad95d947fcbe72fa64e2d27313872cb9a44f53
Udayasaran-kumar <ssaran3091998@gmail.com>
2024-12-07 22:48:34 +0530

    Create q14.js

---
+++ q14.js
@@ -0,0 +1,13 @@
+function objectmerger(){const profile = { name: "Charlie", age: 29, address: { city: "San Francisco", zipcode: "94101" } };
+let g={...profile.address}
+let newprof={name:personname,age:personage,}=profile;
+var a={name:personname,age:personage,...g}
+//console.log(a)
+const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
+var b={...updates.address};
+let newad={age:newage}=updates;
+var c={age:newage,...b};
+const returnedTarget = Object.assign(a, c);
+return returnedTarget;
+};
+console.log(objectmerger());
